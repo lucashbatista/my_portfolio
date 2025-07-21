a55009da540c5c69725c5206451a6e6256306816
Lucas Batista <lucashbatista@icloud.com>
2025-07-21 18:29:48 -0500

    load packages project1

--- src/css/send-email.php
+++ src/css/send-email.php
@@ -19,3 +19,15 @@ $mail -> Host = "smtp.example.com"
 $mail -> SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
 $mail -> Port = 587;
 
+$mail -> Username = "lucashbatista@icloud.com";
+$mail -> Password = ""
+
+$mail -> setFrom($email, $name);
+$mail -> addAddress("lucashbatista@icloud.com", "Lucas")
+
+$mail -> Subject = $subject;
+$mail -> Body = $message;
+
+$mail -> send();
+
+echo
