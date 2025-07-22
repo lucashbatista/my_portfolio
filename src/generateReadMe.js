a2097f5fe6f408951cd93225d743490a24481364
Lucas Batista <lucashbatista@icloud.com>
2025-07-22 17:44:13 -0500

    src/README.html

--- src/generateReadMe.js
+++ src/generateReadMe.js
@@ -4,4 +4,4 @@ var fs = require('fs');
 var readMe = fs.readFileSync('README.md', 'utf-8');
 var markdownReadMe = marked(readMe);
 
-fs.writeFileSync('./site/README.html', markdownReadMe);
+fs.writeFileSync('src/README.html', markdownReadMe);
