1c32f08d9a74c8831416eccb598d4dc70179059a
Lucas Batista <lucashbatista@icloud.com>
2025-07-31 19:07:31 -0500

    .tiles-container

---
+++ src/send-email.php
@@ -0,0 +1,11 @@
+<?php
+    // hashing = transforming sensitive data (password)
+    //           into letters, numbers, and/or symbol
+    //           via a mathematical process. (similar to )
+
+    $password = "pizza123"
+
+    $hash = password_hash
+
+
+?> 
